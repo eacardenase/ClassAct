fcdc2e357e9eb716368d9e702ab13ef0fdea9cda
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-26 20:26:51 -0500

    feat: implement hiearchy and method lists

--- ClassAct/main.m
+++ ClassAct/main.m
@@ -8,8 +8,39 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
+NSArray *BNRHierarchyForClass(Class cls) {
+    NSMutableArray *classHierarchy = [NSMutableArray array];
+    
+    for (Class c = cls; c != Nil; c = class_getSuperclass(c)) {
+        NSString *className = NSStringFromClass(c);
+        
+        [classHierarchy insertObject:className atIndex:0];
+    }
+    
+    return classHierarchy;
+}
+
+NSArray *BNRMethodsForClass(Class cls) {
+    unsigned int methodCount = 0;
+    Method *methodList = class_copyMethodList(cls, &methodCount);
+    NSMutableArray *methodArray = [NSMutableArray array];
+    
+    for (int m = 0; m < methodCount; m++) {
+        Method currentMethod = methodList[m];
+        SEL methodSelector = method_getName(currentMethod);
+        
+        [methodArray addObject:NSStringFromSelector(methodSelector)];
+    }
+    
+    free(methodList);
+    
+    return methodArray;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        NSMutableArray *runtimeClassesInfo = [NSMutableArray array];
+        
         unsigned int classCount = 0;
         
         Class *classList = objc_copyClassList(&classCount);
@@ -18,10 +49,24 @@ int main(int argc, const char * argv[]) {
             Class currentClass = classList[i];
             NSString *className = NSStringFromClass(currentClass);
             
-            NSLog(@"%@", className);
+            NSArray *hierarchy = BNRHierarchyForClass(currentClass);
+            NSArray *methods = BNRMethodsForClass(currentClass);
+            NSDictionary *classInfoDict = @{
+                @"classname": className,
+                @"hierarchy": hierarchy,
+                @"methods": methods
+            };
+            
+            [runtimeClassesInfo addObject:classInfoDict];
         }
         
         free(classList);
+        
+        NSSortDescriptor *alphaAsc = [NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES];
+        NSArray *sortedArray = [runtimeClassesInfo sortedArrayUsingDescriptors:@[alphaAsc]];
+        
+        NSLog(@"There are %ld classes registered with this program's Runtime.", sortedArray.count);
+        NSLog(@"%@", sortedArray);
     }
     return 0;
 }
